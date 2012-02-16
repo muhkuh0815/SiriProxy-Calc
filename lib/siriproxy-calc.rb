8aa10109d77aee409c68fa4aa0ec463e84a4c251
Michael <ullrichm@gmx.at>
2012-02-16 11:59:15 +0100

    testing loop

--- lib/siriproxy-calc.rb
+++ lib/siriproxy-calc.rb
@@ -54,6 +54,9 @@ class SiriProxy::Plugin::Calc < SiriProxy::Plugin
 
     def cleanup(doc)
     doc = doc.to_s
+    	while doc.match(/(zweier)/)
+ 			doc = doc.sub( "zweier","2")
+ 		end
  		while doc.match(/(eins)/)
  			doc = doc.sub( "eins","1")
  		end
@@ -96,6 +99,7 @@ class SiriProxy::Plugin::Calc < SiriProxy::Plugin
 	doc = doc.sub( ".", "" )
 	doc = doc.sub( ",", "." )
 	doc = doc.strip
+	#doc = doc.to_f
  	return doc
  	end
 
@@ -120,6 +124,76 @@ class SiriProxy::Plugin::Calc < SiriProxy::Plugin
 		@res = res.to_s.sub( ".", "," )
 		return rer,ror
 	end
+	
+	def ausgabe()
+	re = 0
+	resp = ask @vor.to_s + " + " + @nach.to_s + " = " + @res.to_s, spoken: " " + @res.to_s
+		while re == 0
+			if resp.match(/(Blues)/)
+				ph = resp.match(/(Blues)/)
+				phh = ph.post_match.strip
+				cal = "+"
+				res = @res
+				replus(@res, phh, cal)
+				resp = ask res + " " + cal + " " + @nach + " = " + @res, spoken:  cal + " " + @nach + " = " + @res
+			elsif resp.match(/(\+)/)
+				ph = resp.match(/(\+)/)
+				phh = ph.post_match.strip
+				cal = "+"
+				res = @res
+				replus(@res, phh, cal)
+				resp = ask res + " " + cal + " " + @nach + " = " + @res, spoken:  cal + " " + @nach + " = " + @res
+			elsif resp.match(/(-)/)
+				ph = resp.match(/(-)/)
+				phh = ph.post_match.strip
+				cal = "-"
+				res = @res
+				replus(@res, phh, cal)
+				resp = ask res.to_s + " " + cal + " " + @nach + " = " + @res, spoken: " minus " + @nach + " = " + @res
+			elsif resp.match(/(×)/)  # × not x
+				ph = resp.match(/(×)/)
+				phh = ph.post_match.strip
+				cal = "*"
+				replus(@res, phh, cal)
+				resp = ask res.to_s + " " + cal + " " + @nach + " = " + @res, spoken: " mal " + @nach + " = " + @res
+			elsif resp.match(/(mal)/i)  # × not x
+				ph = resp.match(/(mal)/i)
+				phh = ph.post_match.strip
+				print phh 
+				cal = "*"
+				replus(@res, phh, cal)
+				resp = ask res.to_s + " " + cal.to_s + " " + @nach.to_s + " = " + @res.to_s, spoken: " mal " + @nach.to_s + " = " + @res.to_s
+			elsif resp.match(/(\/)/)  # × not x
+				ph = resp.match(/(\/)/)
+				phh = ph.post_match.strip
+				cal = "/"
+				replus(@res, phh, cal)
+				resp = ask res + " " + cal + " " + @nach + " = " + @res, spoken: "durch " + @nach + " = " + @res
+			elsif resp.match(/(dividiert durch)/i)  # × not x
+				ph = resp.match(/(dividiert durch)/i)
+				phh = ph.post_match.strip
+				cal = "/"
+				replus(@res, phh, cal)
+				resp = ask res + " " + cal + " " + @nach + " = " + @res, spoken: "durch " + @nach + " = " + @res
+			elsif resp.match(/(durch)/i)  # × not x
+				ph = resp.match(/(durch)/i)
+				phh = ph.post_match.strip
+				cal = "/"
+				replus(@res, phh, cal)
+				resp = ask res + " " + cal + " " + @nach + " = " + @res, spoken: "durch " + @nach + " = " + @res
+			elsif resp.match(/(ist|gleich)/i)
+				say "Ergebniss = " + @res
+				re = 1
+			elsif resp.match(/(=)/)
+				say "Ergebniss = " + @res
+				re = 1
+			else
+				say "Ergebniss = " + @res
+				re = 1
+				#request_completed
+			end
+		end 
+	end
     
 listen_for /(Rechne|berechne) (.*)/i do |phrose,phrase|
 	@vor = 0
@@ -128,29 +202,46 @@ listen_for /(Rechne|berechne) (.*)/i do |phrose,phrase|
 	ss = ""
 	ph = phrase.insert(0, " ")
 	ph = ph.to_s
-	if phrase.match(/( \+ )/) 
-		ma = phrase.match(/( \+ )/)
+	if phrase.match(/( bloß )/)
+		phrase= phrase.sub( " bloß ", " + " )
+	end
+	if phrase.match(/( \+)/) 
+		ma = phrase.match(/( \+)/)
 		vor = ma.pre_match.strip
 		nach = ma.post_match.strip
 		cal = "+"
+		print cal.to_s
 		replus(vor,nach,cal)
-		say @vor.to_s + " + " + @nach.to_s + " = " + @res.to_s, spoken: " " + @res.to_s
-	elsif phrase.match(/( × )/)  # × not x
-		ma = phrase.match(/( × )/)
+		ausgabe()
+	elsif phrase.match(/(×)/)  # × not x
+		ma = phrase.match(/(×)/)
+		vor = ma.pre_match.strip
+		print "--v-"
+		print vor
+		print "--n"
+		nach = ma.post_match.strip
+		print nach
+		cal = "*"
+		print "rechnungstart"
+		replus(vor,nach,cal)
+		print "rechnungsende"
+		say @vor.to_s + " x " + @nach.to_s + " = " + @res.to_s, spoken: " " + @res.to_s
+	elsif phrase.match(/(mal)/)
+		ma = phrase.match(/(mal)/)
 		vor = ma.pre_match.strip
 		nach = ma.post_match.strip
 		cal = "*"
 		replus(vor,nach,cal)
 		say @vor.to_s + " x " + @nach.to_s + " = " + @res.to_s, spoken: " " + @res.to_s
-	elsif phrase.match(/(mal )/)
-		ma = phrase.match(/(mal )/)
+	elsif phrase.match(/(Mai)/)
+		ma = phrase.match(/(Mai)/)
 		vor = ma.pre_match.strip
 		nach = ma.post_match.strip
 		cal = "*"
 		replus(vor,nach,cal)
 		say @vor.to_s + " x " + @nach.to_s + " = " + @res.to_s, spoken: " " + @res.to_s
-	elsif phrase.match(/( - )/)
-		ma = phrase.match(/( - )/)
+	elsif phrase.match(/(-)/)
+		ma = phrase.match(/(-)/)
 		vor = ma.pre_match.strip
 		nach = ma.post_match.strip
 		cal = "-"
